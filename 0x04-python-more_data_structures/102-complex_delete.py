30fd03c6421dafe5e0176cf1e7842f8f323d796f
DennisKGyamfi <ngyamfi81@gmail.com>
2023-06-15 11:06:46 +0000

    102-complex_delete.py

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def complex_delete(a_dictionary, value):
+    dict_copy = my_dict.copy()
+
+    for k, v in dict_copy.items():
+        if value == v:
+            del a_dictionary[k]
+    return (a_dictionary)

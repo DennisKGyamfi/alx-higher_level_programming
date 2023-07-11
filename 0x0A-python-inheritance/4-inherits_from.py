8939d68632555d2f5b18127545ed1e09baf2cbba
DennisKGyamfi <ngyamfi81@gmail.com>
2023-07-11 10:41:34 +0000

    0x0A-python-inheritance

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -5,5 +5,7 @@ this here module contains the inherits_from function
 
 
 def inherits_from(obj, a_class):
-    """returns true if the object is an instance of a subclass """
-    return(issubclass(type(obj), a_class) and type(obj) != a_class)
+
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    return False
